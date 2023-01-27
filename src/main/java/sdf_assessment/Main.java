8b17c9c179d1dc86df7d617c97413cd6c192c4b1
Charleswcl <wang90cl@hotmail.com>
2023-01-27 14:50:18 +0800

    read file line by line

---
+++ src/main/java/sdf_assessment/Main.java
@@ -0,0 +1,15 @@
+package sdf_assessment;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        
+
+
+    }
+}
